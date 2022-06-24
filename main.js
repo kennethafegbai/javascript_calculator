c804e0a03a2f5933fa157ba407f2b27ca6147ffd
Kenneth Afegbai <65070992+kennethafegbai@users.noreply.github.com>
2022-06-24 18:47:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+// program for a simple calculator
 
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+// variable for answer
+let result;
+
+// logic
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the answer
+console.log(`${number1} ${operator} ${number2} = ${result}`);
